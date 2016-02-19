735d69005188e42b864de8a49456c5299855aa0b
Jan Paw <jpaw@virtuslab.com>
2016-02-19 09:40:20 +0100

    Device struct comments for exported methods

--- driver/driver.go
+++ driver/driver.go
@@ -5,12 +5,14 @@ import (
 	"github.com/explicite/i2c"
 )
 
+// Driver structure for handling i2c standard functions.
 type Driver struct {
 	bus    *i2c.Bus
 	addr   byte
 	active bool
 }
 
+// Load(addr, busNo) initialize i2c bus and save device address.
 func (d *Driver) Load(addr byte, busNo byte) error {
 	bus, err := i2c.NewBus(busNo)
 	if err != nil {
@@ -20,14 +22,17 @@ func (d *Driver) Load(addr byte, busNo byte) error {
 	return nil
 }
 
+// Read(cmd, length) read data[length] from device register.
 func (d *Driver) Read(cmd byte, length byte) (list []byte, err error) {
 	return d.bus.Read(d.addr, cmd, length)
 }
 
+// Write(cmd, list) write data to device register.
 func (d *Driver) Write(cmd byte, list ...byte) (err error) {
 	return d.bus.Write(d.addr, cmd, list...)
 }
 
+// On() device - TODO should be implemented locking
 func (d *Driver) On() error {
 	if d.active == true {
 		return errors.New("device is on")
@@ -36,6 +41,7 @@ func (d *Driver) On() error {
 	return nil
 }
 
+// Off() device - TODO should be implemented locking
 func (d *Driver) Off() error {
 	if d.active != false {
 		return errors.New("device is off")
